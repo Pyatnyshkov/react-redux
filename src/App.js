f316a16cb3606ced750b37704ac368ddbc56f927
Николай Пятнышков <n.piatnyshkov@egopay.ru>
2021-02-17 00:19:31 +0300

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="container pt-3">
+      <div className="row">
+        <div className="col">
+          FORM
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          POSTS
+        </div>
+        <div className="col">
+          Fetch posts
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
